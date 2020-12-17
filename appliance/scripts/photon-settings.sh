f96177cfc96a822900a0c740bf330cfa847cc27e
dimitar <dmilov@vmware.com>
2020-12-17 11:57:50 +0200

    Update Photon OS repo urls before update.

--- appliance/scripts/photon-settings.sh
+++ appliance/scripts/photon-settings.sh
@@ -7,6 +7,10 @@
 echo '> Disable IPv6'
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 
+pushd /etc/yum.repos.d/
+sed  -i 's/dl.bintray.com\/vmware/packages.vmware.com\/photon\/$releasever/g' photon.repo photon-updates.repo photon-extras.repo photon-debuginfo.repo
+popd
+
 echo '> Applying latest Updates...'
 tdnf -y update
 
