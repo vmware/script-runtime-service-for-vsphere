0feb01654a9365f0e2b154739640a9c208279703
kamennikolov <kamen_bestsoft@yahoo.com>
2021-02-26 17:00:05 +0200

    VCVA Bash client added (#59)
    * VCVA Bash client added
    
    Co-authored-by: Kamen Nikolov <knikolov@vmware.com>

---
+++ clients/vcva/ExecutePowerCLIScript.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+. /root/config.sh
+
+encodedauthinfo=$(echo -n $username:$password | base64)
+
+#login to SRS
+srsapikey=$(curl -X POST "https://${srs_address}/api/auth/login" -H "accept: \*/\*" \
+			-H "Authorization: Basic ${encodedauthinfo}" -H "content-length: 0" -i -k \
+			| grep X-SRS-API-KEY: | awk {'print $2'})
+			 
+#create runspace
+runspaceInfo=$(curl -X POST "https://${srs_address}/api/runspaces" --http1.1 \
+		     -H "accept: application/json" -H "X-SRS-API-KEY:${srsapikey}" \
+			 -H "Content-Type: application/json" \
+			 -d "{\"name\":\"MyRunspace\",\"run_vc_connection_script\":\"true\"}" -k)
+
+#extract the runspace ID				
+runspaceId=$(echo $runspaceInfo | pcregrep -o '"id":.*?[^\\]"' |awk -F':' '{print $2}' | tr --delete \")
+
+#wait for the runspace to become ready
+state=""
+while [ "$state" != "ready" ]
+do
+   sleep 3
+   response=$(curl -X GET "https://${srs_address}/api/runspaces/${runspaceId}" \
+            -H "accept: application/json" -H "X-SRS-API-KEY:${srsapikey}" -k)
+   state=$(echo $response | pcregrep -o '"state":.*?[^\\]"' |awk -F':' '{print $2}' | tr --delete \")   
+done
+
+#read the PowerCLI script
+scriptText=$(</root/$1)
+
+#Initiate script execution
+curl -X POST "https://${srs_address}/api/script-executions" --http1.1 \
+     -H "accept: application/json" -H "X-SRS-API-KEY:${srsapikey}" -H "Content-Type: application/json" \
+	 -d "{\"runspace_id\":\"${runspaceId}\",\"name\":\"My script exectution\",\"script\":\"${scriptText}\"}" -k
---
+++ clients/vcva/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##################################################
+
+srs_address="<SRS_address>"
+username="<VC_username>"
+password="<VC_password>"
+
+##################################################
