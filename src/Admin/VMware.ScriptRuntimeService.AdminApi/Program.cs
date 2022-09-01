5b0bd33b98bd97c40ce78a3440497ffed675592e
Lyuboslav Asenov <lasenov@vmware.com>
2022-09-01 15:44:16 +0300

    configure nlog for admin api

--- src/Admin/VMware.ScriptRuntimeService.AdminApi/Program.cs
+++ src/Admin/VMware.ScriptRuntimeService.AdminApi/Program.cs
@@ -15,7 +15,27 @@ namespace VMware.ScriptRuntimeService.AdminApi
    {
       public static void Main(string[] args)
       {
-         CreateHostBuilder(args).Build().Run();
+         var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+         try {
+            // Copy attached trsuted CA certificates to system trusted certificates if avaiable
+            // Works for photon OS only assuming it will run a photon OS container
+            var expectedAttachedTrustedCADir = "/app/service/trusted-ca-certs";
+            var expectedOSTrustedCADir = "/etc/ssl/certs";
+            if (Directory.Exists(expectedAttachedTrustedCADir) &&
+               Directory.Exists(expectedOSTrustedCADir)) {
+               foreach (var f in Directory.GetFiles(expectedAttachedTrustedCADir)) {
+                  var fi = new FileInfo(f);
+                  File.Copy(f, Path.Combine(expectedOSTrustedCADir, fi.Name));
+               }
+            }
+            CreateHostBuilder(args).Build().Run();
+         } catch (Exception ex) {
+            logger.Error(ex, "Stopped program because of exception");
+            throw;
+         } finally {
+            NLog.LogManager.Shutdown();
+         }
+
       }
 
       public static IWebHostBuilder CreateHostBuilder(string[] args) =>
