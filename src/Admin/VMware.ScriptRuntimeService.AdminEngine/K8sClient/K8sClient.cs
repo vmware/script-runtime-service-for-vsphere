d2a87215e08ce860407bb31018ae7e8cb1c52962
Lyuboslav Asenov <lasenov@vmware.com>
2022-09-07 20:53:44 +0300

    disable log follow

--- src/Admin/VMware.ScriptRuntimeService.AdminEngine/K8sClient/K8sClient.cs
+++ src/Admin/VMware.ScriptRuntimeService.AdminEngine/K8sClient/K8sClient.cs
@@ -204,33 +204,21 @@ namespace VMware.ScriptRuntimeService.AdminEngine.K8sClient {
          using (var stream = _k8sClient.CoreV1.ReadNamespacedPodLog(
             pod.Metadata.Name,
             _namespace,
-            follow: true,
+            follow: false,
             sinceSeconds: sinceSeconds)) {
             using (var reader = new StreamReader(stream)) {
-               return reader.ReadToEnd();
+               return reader
+                  .ReadToEnd()
+                  .Trim()
+                  .Replace("\\n", System.Environment.NewLine)
+                  .Replace("\u001b[40m\u001b[37mtrce\u001b[39m\u001b[22m\u001b[49m", "trce")
+                  .Replace("\u001b[40m\u001b[37mdbug\u001b[39m\u001b[22m\u001b[49m", "dbug")
+                  .Replace("\u001b[40m\u001b[32minfo\u001b[39m\u001b[22m\u001b[49m", "info")
+                  .Replace("\u001b[40m\u001b[1m\u001b[33mwarn\u001b[39m\u001b[22m\u001b[49m", "warn")
+                  .Replace("\u001b[41m\u001b[30mfail\u001b[39m\u001b[22m\u001b[49m", "fail")
+                  .Replace("\u001b[41m\u001b[1m\u001b[37mcrit\u001b[39m\u001b[22m\u001b[49m", "crit");
             }
          }
-
-         //   var buffer = new byte[8192];
-
-         //int count;
-         //do {
-         //   count = stream.Read(buffer, 0, buffer.Length);
-         //   _logger.LogDebug($"{count} bytes read from {pod.Metadata.Name} log.");
-
-         //   if (count > 0) {
-         //      var tmpString = Encoding.Default.GetString(buffer, 0, count);
-
-         //      yield return tmpString
-         //         .Trim()
-         //         .Replace("\u001b[40m\u001b[37mtrce\u001b[39m\u001b[22m\u001b[49m", "trce")
-         //         .Replace("\u001b[40m\u001b[37mdbug\u001b[39m\u001b[22m\u001b[49m", "dbug")
-         //         .Replace("\u001b[40m\u001b[32minfo\u001b[39m\u001b[22m\u001b[49m", "info")
-         //         .Replace("\u001b[40m\u001b[1m\u001b[33mwarn\u001b[39m\u001b[22m\u001b[49m", "warn")
-         //         .Replace("\u001b[41m\u001b[30mfail\u001b[39m\u001b[22m\u001b[49m", "fail")
-         //         .Replace("\u001b[41m\u001b[1m\u001b[37mcrit\u001b[39m\u001b[22m\u001b[49m", "crit");
-         //   }
-         //} while (count > 0);
       }
    }
 }
