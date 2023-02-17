33b0908cf1c6ea3509b6eb99c2ca9f7ad707de97
Lyuboslav Asenov <lasenov@vmware.com>
2023-02-17 14:09:23 +0200

    add entiryly async interface

--- src/Admin/VMware.ScriptRuntimeService.AdminEngine/VCRegistration/VCTrustedCertificatesCollector.cs
+++ src/Admin/VMware.ScriptRuntimeService.AdminEngine/VCRegistration/VCTrustedCertificatesCollector.cs
@@ -64,24 +64,8 @@ namespace VMware.ScriptRuntimeService.AdminEngine.VCRegistration {
       }
 
       public IEnumerable<string> GetEncodedTrustedCertificates() {
-         _logger.LogInformation(
-                  string.Format(Resources.PerofomingOperation, Resources.EncodedTrustedCertificatesRetrieval));
-         var task = Task.Run<IEnumerable<string>>(async () => {
-            using (var client = GetHttpClient()) {
-               await AuthenticateClientAsync(client);
 
-               List<string> certs = new List<string>();
-               foreach (var chain in await GetTrustedRootChainsChainsAsync(client)) {
-                  foreach (var cert in await GetTrustedCertificateAsync(client, chain)) {
-                     certs.Add(cert);
-
-                     _logger.LogDebug($"Trusted CA cert: {cert}");
-                  }
-               }
-
-               return certs;
-            }
-         });
+         var task = Task.Run(GetEncodedTrustedCertificatesAsync);
 
          try {
             task.Wait();
@@ -91,8 +75,27 @@ namespace VMware.ScriptRuntimeService.AdminEngine.VCRegistration {
          }
       }
 
+      public async Task<IEnumerable<string>> GetEncodedTrustedCertificatesAsync() {
+         _logger.LogInformation(
+                  string.Format(Resources.PerofomingOperation, Resources.EncodedTrustedCertificatesRetrieval));
+
+         using (var client = GetHttpClient()) {
+            await AuthenticateClientAsync(client);
+
+            List<string> certs = new List<string>();
+            foreach (var chain in await GetTrustedRootChainsChainsAsync(client)) {
+               foreach (var cert in await GetTrustedCertificateAsync(client, chain)) {
+                  certs.Add(cert);
+
+                  _logger.LogDebug($"Trusted CA cert: {cert}");
+               }
+            }
+
+            return certs;
+         }
+      }
+
       private async Task<IEnumerable<string>> GetTrustedCertificateAsync(HttpClient client, string chain) {
-         // https://{api_host}/api/vcenter/certificate-management/vcenter/trusted-root-chains/{chain}
          using (var response = await client.GetAsync($"/api/vcenter/certificate-management/vcenter/trusted-root-chains/{chain}")) {
             response.EnsureSuccessStatusCode();
 
@@ -115,21 +118,9 @@ namespace VMware.ScriptRuntimeService.AdminEngine.VCRegistration {
 
             return result;
          }
-
-         //{
-         //   "cert_chain": {
-         //      "cert_chain": [
-         //        "-----BEGIN CERTIFICATE-----\nMIIFOzCCA6OgAwIBAgIJAOrjMkQMNfJUMA0GCSqGSIb3DQEBCwUAMIGoMQswCQYD\nVQQDDAJDQTEXMBUGCgmSJomT8ixkARkWB3ZzcGhlcmUxFTATBgoJkiaJk/IsZAEZ\nFgVsb2NhbDELMAkGA1UEBhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExKjAoBgNV\nBAoMIXNjMi0xMC0xODUtMjI1LTE5OS5lbmcudm13YXJlLmNvbTEbMBkGA1UECwwS\nVk13YXJlIEVuZ2luZWVyaW5nMB4XDTIzMDIxMDEyMDAzNVoXDTMzMDIwNzEyMDAz\nNVowgagxCzAJBgNVBAMMAkNBMRcwFQYKCZImiZPyLGQBGRYHdnNwaGVyZTEVMBMG\nCgmSJomT8ixkARkWBWxvY2FsMQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZv\ncm5pYTEqMCgGA1UECgwhc2MyLTEwLTE4NS0yMjUtMTk5LmVuZy52bXdhcmUuY29t\nMRswGQYDVQQLDBJWTXdhcmUgRW5naW5lZXJpbmcwggGiMA0GCSqGSIb3DQEBAQUA\nA4IBjwAwggGKAoIBgQDAyDKYVYwiCMZDSAgxHu595pU/W5pVFMv4gA01z2XfrOZY\nSuIQFY2PTtxjMM7p/3+oPw/sWXhO6ig94jguBZVselu/ksW+N2U+2nUJoScdGGio\nfGvXsV0DmJ0nMfdnSpVmY6R1ImIqZkRjonR4qpHGhzRjqjzSL/hU1Djiez+pIaKW\nugQ9QCFO+DVwiZ2OQqNESPOPQHera0bn4qzJSAjjibsyaoI7MCqhC0cM3oIZ8xLK\nq4QoP0fhUlv8fGLSb3gztO9QD3GGGZf/3nb3Xkf4l1TszKljzVMEjJbztnTiNIHj\nkRd1Am0X3DWpQl2yI6qwOU/Jbt7gVVad+7c9lmxnHVrMjX08pNF6A5k+X7MPg3CS\nWVcfs2LX/J41bTfOQpCgWYZnsh7OfSVlIDthfwh3f8MvSHc+JkACCJwVHTxR4uNW\nER48InN6p2/V+1GglliV/fo9ZMW9e6fI5EwQbEBGScOvpzkV59ABsl4SE0M7J8WT\n3AFrg+hO/cZkQACuTL0CAwEAAaNmMGQwHQYDVR0OBBYEFLobb584cG5Pqq+KoKFq\nfh5gxOv+MB8GA1UdEQQYMBaBDmVtYWlsQGFjbWUuY29thwR/AAABMA4GA1UdDwEB\n/wQEAwIBBjASBgNVHRMBAf8ECDAGAQH/AgEAMA0GCSqGSIb3DQEBCwUAA4IBgQCA\nynTFEvpjnBcrV7gjI2GBbVwK4kJqRURWbFMUZO/pgUKgT9pA7f2bOuoVgnLn0suY\nXXTPK0hBRQ5CsPvgUPuq2B3u9rOf25WMlnasDWqG3NPi+8GFsYKRr2nQecClI36q\nPmafcQ+cao3fm5OEXrecZRb5tG313p0YtlYgu9dJRjIw9CcJHrDsNKvj5WMyqrD0\ndHWlzq+DvZSZRLbk+ddn1QaU9D6SXYY+t8rSH+XvFj+RCgNZ1zWi4oxtx22rfMW6\n2dDooR4tRZ4FdtD9OOBTUg8bZs78hgBHD+2Hi5pH+HobO8yYvPs1BHPiAL/7/GCi\nRviQ205ZFw0hbMn8PMOQrHilFA1jx4NxqRlEWSBSOFVrmuB2YftHit77rzwfvHCl\nZ27QNsJu6u+wbGb9IMyipNsWWZ8sr7dAegdm3JkgJ9Ig89BfxsIfGXykuKrVYyx3\nrbhtJpljRHRdB0in5pvu36XCTrUWKcYoEjPSPrAr8MEd6hRqx1AlHglo41lQvNM=\n-----END CERTIFICATE-----\n-----BEGIN X509 CRL-----\nMIICpDCCAQwCAQEwDQYJKoZIhvcNAQELBQAwgagxCzAJBgNVBAMMAkNBMRcwFQYK\nCZImiZPyLGQBGRYHdnNwaGVyZTEVMBMGCgmSJomT8ixkARkWBWxvY2FsMQswCQYD\nVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEqMCgGA1UECgwhc2MyLTEwLTE4\nNS0yMjUtMTk5LmVuZy52bXdhcmUuY29tMRswGQYDVQQLDBJWTXdhcmUgRW5naW5l\nZXJpbmcXDTIzMDIxNTEzNTAzNVoXDTIzMDMxNzEzNTAzNVqgLzAtMAoGA1UdFAQD\nAgELMB8GA1UdIwQYMBaAFLobb584cG5Pqq+KoKFqfh5gxOv+MA0GCSqGSIb3DQEB\nCwUAA4IBgQAGIImskOv8S2F140sGtP/mZ5MossDuks1cfbAC6nwsrPBDcwaoqpnl\n317ixIb3BHoW0b3MQydYivzybYehp//HxWOzqFFu0DJtBxEYbFADbxR33mr2O2hu\n8ANT8zTUgosBlheLqUZSH5DumAtiZa/Q1JKUGCG4QuFoMHi5EPfRK5/qdFeOEz+I\n1OELahB+9pyd6kO8EhzmmhN8UFUwImGJhdAR0NdXE7naBEQv2Y/nKhOk8gbJUEAR\nifDNeVdptKQR0cp+rS7ygscU1tfIntzdd9hkVGpSwuPkOK27iSqxjO9OQW6/x57M\nbJc1t39KmbqiwuDf/anJvzyM4sUVPPw4+ZMZdoqkBPW58eRZdk+x5I6kXQf+ZntC\nEvcSdV0JQGC8mR+j+YD36lfLIkL60payKMwohIhEr7Cj2SMVEJRZv3RgHaWi+e6U\nKkgOV2RbFt5wV5A+CczVJVDKPU4Q0wFsV23HIhURXmEDUz1ZLZ6I2vJaj5eJ80za\nYSCgKPcAsxA=\n-----END X509 CRL-----"
-         //     ]
-         //   }
-         //}
-
-
-            // dynamic result = JsonConvert.DeserializeObject<dynamic>(responseBody);
       }
 
       private async Task<IEnumerable<string>> GetTrustedRootChainsChainsAsync(HttpClient client) {
-         // https://{api_host}/api/vcenter/certificate-management/vcenter/trusted-root-chains
          using (var response = await client.GetAsync("/api/vcenter/certificate-management/vcenter/trusted-root-chains")) {
             response.EnsureSuccessStatusCode();
 
@@ -137,14 +128,7 @@ namespace VMware.ScriptRuntimeService.AdminEngine.VCRegistration {
             dynamic result = JsonConvert.DeserializeObject<dynamic>(responseStr);
 
             return ((IEnumerable) result).Cast<dynamic>().Select(r => (string) r.chain);
-         }
-         //[
-         //   {
-         //      "chain": "BA1B6F9F38706E4FAAAF8AA0A16A7E1E60C4EBFE"
-         //   }
-         //]
-
-         // 
+         } 
       }
 
       private HttpClient GetHttpClient() {
@@ -187,8 +171,6 @@ namespace VMware.ScriptRuntimeService.AdminEngine.VCRegistration {
          _logger.LogInformation("Establishing session");
 
          string authHeaderValue = Convert.ToBase64String(Encoding.ASCII.GetBytes(_username + ":" + SecureStringToString(_password)));
-
-         // https://{api_host}/api/session
 
          using (var request = new HttpRequestMessage(HttpMethod.Post, "/api/session")) {
             request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authHeaderValue);
