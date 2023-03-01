35765ab299612c15ac46b19fb2b6a593c07034f0
Lyuboslav Asenov <lasenov@vmware.com>
2023-03-01 10:19:40 +0200

    Revert "remove unnecesery task"
    This reverts commit 067128bb9dcfa29951dd612691450ee9af1d81d3.

--- src/Endpoint/VMware.ScriptRuntimeService.APIGateway/Authentication/Basic/BasicAuthenticationHandler.cs
+++ src/Endpoint/VMware.ScriptRuntimeService.APIGateway/Authentication/Basic/BasicAuthenticationHandler.cs
@@ -38,54 +38,57 @@ namespace VMware.ScriptRuntimeService.APIGateway.Authentication.Basic {
          _logger = _loggerFactory.CreateLogger(typeof(BasicAuthenticationHandler));
       }
 
-      protected override Task<AuthenticateResult> HandleAuthenticateAsync() {
-         AuthenticateResult result = null;
-         try {
-            _logger.Log(LogLevel.Debug, "Handle Basic Authentication Start");
+      protected override async Task<AuthenticateResult> HandleAuthenticateAsync() {
+         return await Task.Run(
+         () => {
+            AuthenticateResult result = null;
+            try {
+               _logger.Log(LogLevel.Debug, "Handle Basic Authentication Start");
 
-            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
-            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
-            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
-            var username = credentials[0];
-            SecureString password = new SecureString();
-            foreach (char c in credentials[1]) {
-               password.AppendChar(c);
+               var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+               var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+               var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+               var username = credentials[0];
+               SecureString password = new SecureString();
+               foreach (char c in credentials[1]) {
+                  password.AppendChar(c);
+               }
+
+               _logger.Log(LogLevel.Debug, "Issue solution HoK token for the user authorized with credentials");
+               _logger.Log(LogLevel.Debug, $"STS Settings SolutionOwnerId: {Options.SolutionOwnerId}");
+               _logger.Log(LogLevel.Debug, $"STS Settings SolutionServiceId: {Options.SolutionServiceId}");
+               _logger.Log(LogLevel.Debug, $"STS Settings SolutionUserSigningCertificatePath: {Options.SolutionUserSigningCertificatePath}");
+               _logger.Log(LogLevel.Debug, $"STS Settings Realm: {Options.Realm}");
+               var sessionHoKToken = new SolutionStsClient(_loggerFactory, Options)
+                  .IssueSolutionTokenByUserCredential(
+                     username,
+                     password);
+
+               // Issue session for the subject of the SAML token
+               var sessionToken = SessionToken.Issue(new SamlToken(sessionHoKToken.OuterXml));
+               
+               Response.Headers.Add(APIGatewayResources.SRSAuthorizationHeader, sessionToken.SessionId);
+               sessionToken.HoKSamlToken = new SamlToken(sessionHoKToken.OuterXml);
+
+               var claims = new[] {
+                  new Claim(ClaimTypes.Name, sessionToken.UserName),
+               };
+               var identity = new ClaimsIdentity(claims, AuthenticationScheme);
+               var principal = new ClaimsPrincipal(identity);
+
+               result = AuthenticateResult.Success(
+                  new AuthenticationTicket(principal, AuthenticationScheme));
+               
+            } catch (Exception exc) {
+               _logger.LogError(exc, "Basic Authorization failure");
+               try {
+                  _logger.LogTrace($"Failed Authorization header '{Request.Headers["Authorization"]}'");
+               } catch { }
+               result = AuthenticateResult.Fail(exc.Message);
             }
 
-            _logger.Log(LogLevel.Debug, "Issue solution HoK token for the user authorized with credentials");
-            _logger.Log(LogLevel.Debug, $"STS Settings SolutionOwnerId: {Options.SolutionOwnerId}");
-            _logger.Log(LogLevel.Debug, $"STS Settings SolutionServiceId: {Options.SolutionServiceId}");
-            _logger.Log(LogLevel.Debug, $"STS Settings SolutionUserSigningCertificatePath: {Options.SolutionUserSigningCertificatePath}");
-            _logger.Log(LogLevel.Debug, $"STS Settings Realm: {Options.Realm}");
-            var sessionHoKToken = new SolutionStsClient(_loggerFactory, Options)
-               .IssueSolutionTokenByUserCredential(
-                  username,
-                  password);
-
-            // Issue session for the subject of the SAML token
-            var sessionToken = SessionToken.Issue(new SamlToken(sessionHoKToken.OuterXml));
-               
-            Response.Headers.Add(APIGatewayResources.SRSAuthorizationHeader, sessionToken.SessionId);
-            sessionToken.HoKSamlToken = new SamlToken(sessionHoKToken.OuterXml);
-
-            var claims = new[] {
-               new Claim(ClaimTypes.Name, sessionToken.UserName),
-            };
-            var identity = new ClaimsIdentity(claims, AuthenticationScheme);
-            var principal = new ClaimsPrincipal(identity);
-
-            result = AuthenticateResult.Success(
-               new AuthenticationTicket(principal, AuthenticationScheme));
-               
-         } catch (Exception exc) {
-            _logger.LogError(exc, "Basic Authorization failure");
-            try {
-               _logger.LogTrace($"Failed Authorization header '{Request.Headers["Authorization"]}'");
-            } catch { }
-            result = AuthenticateResult.Fail(exc.Message);
-         }
-
-         return Task.FromResult(result);
+            return result;
+         });
       }
 
       protected override async Task HandleChallengeAsync(AuthenticationProperties properties) {
